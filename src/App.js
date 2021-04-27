88b9636ef32e9ec9d9b641f629c91b444d98724b
nafeyah <nafeyahthenx@gmail.com>
2021-04-27 23:33:42 +0530

    wq
    	git branch -M main#

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React ,{ useEffect ,useState}from "react";
 import './App.css';
+import Chat from "./Chat";
+import Sidebar from "./Sidebar";
+import Pusher from "pusher-js";
+import axios from './axios';
 
 function App() {
+  const [messages,setMessages]=useState([]);
+  useEffect(()=>{
+    axios.get('/messages/sync').then((response)=>{
+      
+      setMessages(response.data);
+    });
+
+  },[]);
+
+
+
+  
+  useEffect(() => {
+    var pusher = new Pusher('66ac853436a082c004d3', {
+      cluster: 'ap2'
+    });
+
+    var channel = pusher.subscribe('messages');
+    channel.bind('inserted', function(data) {
+      alert(JSON.stringify(data));
+      setMessages([...messages,data]);
+    });
+    return () => {
+    channel.unbind_all();
+    channel.unsubscribe();
+  }
+  }, [messages]);
+
+  
+  console.log(messages);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__body">
+      <Sidebar />
+      <Chat messages={messages}/>
+      
+      </div>
     </div>
   );
 }
